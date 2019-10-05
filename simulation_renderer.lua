5cba5e078ab0dc4b8770aa4d7568e0d9752d3fcb
Ben Buckton <ben.buckton@factorio.com>
2019-10-05 14:54:58 +0200

    added visualization for connections

--- simulation_renderer.lua
+++ simulation_renderer.lua
@@ -9,17 +9,20 @@ local render_constants = {
     ["sink_part"] = {0.94,0.99,0.23},
     ["gear"] = {1,0.69,0.23},
     ["sink"] = {0.23,0.49,0.78},
+    ["link"] = {0.3,0.3,0.3},
     ["other"] = {1,1,1},
   }
 }
-
-
 
 renderer.draw = function(state)
   for _, component in pairs(state.all_components) do
     love.graphics.setColor(render_constants.colors[component.type] or render_constants.colors['other'])
     love.graphics.circle('line',component.position.x,component.position.y,component.size * render_constants.size_mod,50)
     love.graphics.print(tostring(component.current_speed),component.position.x,component.position.y)
+    if component.child then
+      love.graphics.setColor(constants.colors["link"])
+      love.graphics.line(component.position.x,component.position.y,component.child.position.x,component.child.position.y)
+    end
   end
 
   love.graphics.rectangle('fill', constants.screen_w - constants.right_bar, 0, constants.right_bar, constants.screen_h)
