e5e4c8af22964ed3a8def35fbc2483c3e73b3484
Tom Mason <wheybags@wheybags.com>
2019-10-05 12:54:38 +0200

    add first version of simulation code

--- main.lua
+++ main.lua
@@ -1,7 +1,23 @@
 local placement = require('gear_placement')
 require('registry')
+local simulation = require('simulation')
+
+
+local test_gear
+
+local state
 
 function love.load()
+  state = simulation.create_state()
+
+  local source = simulation.add_source(state, "basic_source", 2, 5, {x = 0, y = 50})
+  local sink = simulation.add_sink(state, "basic_sink", {x = 50, y = 50}, 1)
+  simulation.add_sink_part(sink, "basic_sink_part_1", 1, 4,6, {x=50,y=55})
+
+  test_gear = simulation.add_gear(state, 1, {x=25, y=50})
+  simulation.connect(source, test_gear)
+  simulation.connect(test_gear, sink.components[1])
+
   local screen_width, screen_height = love.graphics.getDimensions()
   for _=1, 100 do
     table.insert(circles,{shape='circle',x=math.random()*screen_width,y=math.random()*screen_height,radius=math.random(1,10)})
@@ -48,5 +64,8 @@ function love.quit()
 end
 
 function love.update(dt)
+  simulation.update(state)
 
+  --print(test_gear.current_speed)
+  --print(state.sinks[1].components[1].current_speed)
 end
---
+++ simulation.lua
@@ -0,0 +1,223 @@
+local simulation = {}
+
+simulation.create_state = function()
+  return
+  {
+    sources = {},
+    sinks = {},
+    all_components = {},
+    money = 0,
+  }
+end
+
+simulation.add_source = function(state, name, size, speed, position)
+  local new_source =
+  {
+    type = "source",
+    name = name,
+    size = size,
+    speed = speed,
+    position = position,
+    child = nil,
+  }
+
+  table.insert(state.sources, new_source)
+  table.insert(state.all_components, new_source)
+
+  return new_source
+end
+
+simulation.update_source = function(source)
+  assert(source.type == "source")
+
+  if source.child then
+    simulation.update_recursive(source.child, source.size, source.speed)
+  end
+end
+
+simulation.add_sink = function(state, name, position, money_per_tick)
+  local new_sink =
+  {
+    type = "sink",
+    name = name,
+    position = position,
+    money_per_tick = money_per_tick,
+    components = {},
+    satisfied = false,
+  }
+
+  table.insert(state.sinks, new_sink)
+
+  return new_sink
+end
+
+simulation.update_sink = function(state, sink)
+  assert(sink.type == "sink")
+
+  sink.satisfied = true
+  for _, sink_part in pairs(sink.components) do
+    if not sink_part.satisfied then
+      sink.satisfied = false
+      break
+    end
+  end
+
+  if sink.satisfied then
+    state.money = state.money + sink.money_per_tick
+  end
+end
+
+simulation.add_sink_part = function(sink, name, size, speed_min, speed_max, position)
+  assert(sink.type == "sink")
+
+  local new_sink_component =
+  {
+    type = "sink_part",
+    name = name,
+    size = size,
+    speed_min = speed_min,
+    speed_max = speed_max,
+    position = position,
+    parent = nil,
+    satisfied = false,
+    current_speed = 0,
+  }
+
+  table.insert(sink.components, new_sink_component)
+
+  return new_sink_component
+end
+
+simulation.update_sink_part = function(sink_part, parent_size, parent_speed)
+  assert(sink_part.type == "sink_part")
+
+  sink_part.current_speed = -(parent_speed * (parent_size / sink_part.size))
+  sink_part.satisfied = sink_part.current_speed >= sink_part.speed_min and sink_part.current_speed <= sink_part.speed_max
+end
+
+simulation.add_gear = function(state, size, position)
+  local new_gear =
+  {
+    type = "gear",
+    size = size,
+    position = position,
+    parent = nil,
+    child = nil,
+    current_speed = 0,
+  }
+
+  table.insert(state.all_components, new_gear)
+
+  return new_gear
+end
+
+simulation.update_gear = function(gear, parent_size, parent_speed)
+  assert(gear.type == "gear")
+
+  gear.current_speed = -(parent_speed * (parent_size / gear.size))
+
+  if gear.child then
+    simulation.update_recursive(gear.child, gear.size, gear.current_speed)
+  end
+end
+
+simulation.can_connect = function (parent, child)
+  return parent.type ~= "sink" and child.type ~= "source" and parent.child == nil and child.parent == nil
+end
+
+simulation.connect = function(parent, child)
+  assert(simulation.can_connect(parent, child))
+  parent.child = child
+  child.parent = parent
+end
+
+
+simulation.update_recursive = function(component, parent_size, parent_speed)
+  if component.type == "source" then
+    simulation.update_source(component)
+  elseif component.type == "sink_part" then
+    simulation.update_sink_part(component, parent_size, parent_speed)
+  elseif component.type == "gear" then
+    simulation.update_gear(component, parent_size, parent_speed)
+  else
+    error("bad component type: " .. component.type)
+  end
+end
+
+simulation.update = function(state)
+  for _, source in pairs(state.sources) do
+    simulation.update_recursive(source, nil, nil)
+  end
+
+  for _, sink in pairs(state.sinks) do
+    simulation.update_sink(state, sink)
+  end
+end
+
+return simulation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
